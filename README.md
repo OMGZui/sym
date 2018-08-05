06897be99a2e4a26d0d83c116c5f80b9a8dd05d7
小粽子 <862275679@qq.com>
2018-08-06 00:03:21 +0800

    2018-08-06 00:03:21

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-Symfony tutorial
+# Symfony tutorial
+
+## deploy
+
+```php
+# install
+composer install
+# edit .env of mysql config
+DATABASE_URL=mysql://db_user:db_password@127.0.0.1:3306/db_name
+# run migrate
+php bin/console doctrine:migrations:migrate
+```
+
+## visit
+```php
+# require web-server
+composer require symfony/web-server-bundle --dev
+# run
+php bin/console server:run
+# visit in browser
+http://localhost:8000/
+```
--- src/Controller/SyController.php
+++ src/Controller/SyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -22,10 +23,28 @@ class SyController extends Controller
     /**
      * @Route("/sy/number", name="sy_number")
      * @Method({"GET"})
+     * @return Response
+     * @throws \Exception
      */
     public function number()
     {
         $num = random_int(1, 1000);
         return new Response("index:{$num}");
+    }
+
+    /**
+     *
+     * @Route("/sy/hTop/{top}", name="sy_hTop")
+     * @Method({"GET"})
+     * @param Request $request
+     * @return Response
+     */
+    public function hTop(Request $request)
+    {
+        $top = $request->get('top');
+        return $this->render('sy/hTop.html.twig', [
+            'top' => $top,
+            'num' => rand(1, 9999)
+        ]);
     }
 }
